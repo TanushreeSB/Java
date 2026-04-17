f8c6d636a112eaeaddf6f093c39044fced4c6d85
Tanushree Borase <tanushreebcs@gmail.com>
2026-04-17 14:14:26 +0530

    Create ReverseNumber.java

---
+++ Day 6 Logical Programming Practice Problem/ReverseNumber.java
@@ -0,0 +1,13 @@
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int num = 1234, reverse = 0;
+
+        while (num != 0) {
+            int rem = num % 10;
+            reverse = reverse * 10 + rem;
+            num /= 10;
+        }
+
+        System.out.println("Reversed: " + reverse);
+    }
+}
